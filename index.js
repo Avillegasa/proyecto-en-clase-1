29af4275cd1cd9833c34f906a8b8c1b4e42856fb
Andres Villegas <andres.villegasa19@gmail.com>
2023-09-07 11:32:02 -0400

    Uso de comandos de js appendchild y create element,textnode y queryselector

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hola mundo');
+const shareDiv = document.createElement('div')
+const cantidad = document.createElement('span')
+const shares = document.createElement('span')
+
+const text = document.createTextNode('1K')
+cantidad.appendChild(text)
+const text2 = document.createTextNode('Shares')
+shares.appendChild(text2)
+
+shareDiv.appendChild(cantidad)
+shareDiv.appendChild(shares)
+
+const stat = document.querySelector('.stat')
+stat.appendChild(shareDiv)
